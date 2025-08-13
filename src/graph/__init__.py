1a429729ac39db565e6e546e6d4d797b92dd6372
Johl Brown <johlbrown@gmail.com>
2025-08-13 21:56:51 +1000

    Test case nodes and new edge types

--- src/graph/__init__.py
+++ src/graph/__init__.py
@@ -1,13 +1,22 @@
 """Graph utilities for representing relationships between legal entities."""
 
 from .ingest import compute_weight, ingest_extrinsic
-from .models import EdgeType, ExtrinsicNode, GraphEdge, GraphNode, LegalGraph, NodeType
+from .models import (
+    CaseNode,
+    EdgeType,
+    ExtrinsicNode,
+    GraphEdge,
+    GraphNode,
+    LegalGraph,
+    NodeType,
+)
 
 __all__ = [
     "EdgeType",
     "GraphEdge",
     "GraphNode",
     "ExtrinsicNode",
+    "CaseNode",
     "LegalGraph",
     "NodeType",
     "ingest_extrinsic",
