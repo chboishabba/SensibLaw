57998e5433b880a3b7a861271324f36423b6c66b
Johl Brown <johlbrown@gmail.com>
2025-10-16 15:14:16 +1000

    Refactor Streamlit console into modular package

---
+++ sensiblaw_streamlit/tabs/__init__.py
@@ -0,0 +1,11 @@
+"""Tab renderers for the SensibLaw Streamlit console."""
+
+from __future__ import annotations
+
+__all__ = [
+    "documents",
+    "text_concepts",
+    "knowledge_graph",
+    "case_comparison",
+    "utilities",
+]
