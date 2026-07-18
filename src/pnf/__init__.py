7503cf286e9ab3f6a3e3a6b455030cea82c1b9a6
Johl Brown <johlbrown@gmail.com>
2026-07-18 23:33:35 +1000

    Export canonical operational binding assembly

--- src/pnf/__init__.py
+++ src/pnf/__init__.py
@@ -12,12 +12,14 @@ from .binding_candidate_sets import (
 from .closure import ClosureContract, assess_pnf_closure
 from .demands import derive_resolution_demands
 from .graph import PNFGraph
-from .reference_binding import (
+from .operational_reference_binding import (
     REFERENCE_BINDING_CONTRACT_REF,
     REFERENCE_REDUCTION_DECLARATION_REF,
+    build_operational_reference_binding_artifacts,
     build_set_valued_binding_artifacts,
-    project_pronominal_reference_arguments,
 )
+from .reference_binding import project_pronominal_reference_arguments
+from .revision_normalization import normalize_factor_revision_artifacts
 
 __all__ = [
     "BindingAccessibilityDeclaration",
@@ -31,8 +33,10 @@ __all__ = [
     "REFERENCE_BINDING_CONTRACT_REF",
     "REFERENCE_REDUCTION_DECLARATION_REF",
     "assess_pnf_closure",
+    "build_operational_reference_binding_artifacts",
     "build_set_valued_binding_artifacts",
     "compact_binding_artifacts",
     "derive_resolution_demands",
+    "normalize_factor_revision_artifacts",
     "project_pronominal_reference_arguments",
 ]
