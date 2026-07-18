c67b16adb88de7c852497d9a32ae72a4a104cb4f
Johl Brown <johlbrown@gmail.com>
2026-07-18 23:11:51 +1000

    Export generic reference binding surface

--- src/pnf/__init__.py
+++ src/pnf/__init__.py
@@ -1,12 +1,38 @@
 """Factorized PNF graph primitives."""
 
-from .graph import PNFGraph
+from .binding_candidate_sets import (
+    BindingAccessibilityDeclaration,
+    BindingCandidateMember,
+    BindingCandidateSet,
+    BindingCompatibilityDeclaration,
+    BindingExclusionSummary,
+    FactorAnchor,
+    compact_binding_artifacts,
+)
 from .closure import ClosureContract, assess_pnf_closure
 from .demands import derive_resolution_demands
+from .graph import PNFGraph
+from .reference_binding import (
+    REFERENCE_BINDING_CONTRACT_REF,
+    REFERENCE_REDUCTION_DECLARATION_REF,
+    build_set_valued_binding_artifacts,
+    project_pronominal_reference_arguments,
+)
 
 __all__ = [
+    "BindingAccessibilityDeclaration",
+    "BindingCandidateMember",
+    "BindingCandidateSet",
+    "BindingCompatibilityDeclaration",
+    "BindingExclusionSummary",
     "ClosureContract",
+    "FactorAnchor",
     "PNFGraph",
+    "REFERENCE_BINDING_CONTRACT_REF",
+    "REFERENCE_REDUCTION_DECLARATION_REF",
     "assess_pnf_closure",
+    "build_set_valued_binding_artifacts",
+    "compact_binding_artifacts",
     "derive_resolution_demands",
+    "project_pronominal_reference_arguments",
 ]
