6ce6e528ee800aca1ef3c930a28a60c3f757f4bd
Johl Brown <johlbrown@gmail.com>
2026-07-21 18:58:27 +1000

    Export granular semantic review coordinates

--- src/pnf/__init__.py
+++ src/pnf/__init__.py
@@ -29,6 +29,11 @@ from .operational_reference_binding import (
     build_set_valued_binding_artifacts,
 )
 from .reference_binding import project_pronominal_reference_arguments
+from .review_coordinates import (
+    SemanticReviewAssessment,
+    SemanticReviewCoordinate,
+    project_review_state,
+)
 from .revision_normalization import normalize_factor_revision_artifacts
 
 __all__ = [
@@ -48,6 +53,8 @@ __all__ = [
     "REFERENCE_REDUCTION_DECLARATION_REF",
     "ReducedFactor",
     "ReductionResidual",
+    "SemanticReviewAssessment",
+    "SemanticReviewCoordinate",
     "assess_pnf_closure",
     "build_operational_reference_binding_artifacts",
     "build_set_valued_binding_artifacts",
@@ -55,6 +62,7 @@ __all__ = [
     "derive_resolution_demands",
     "normalize_factor_revision_artifacts",
     "project_pronominal_reference_arguments",
+    "project_review_state",
     "proposal_build_key",
     "reduce_factor_proposals",
 ]
